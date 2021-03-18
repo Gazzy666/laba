8113e9b59ce1e65b21444b8e54e424ed335c4ffb
DaneArt <45368154+DaneArt@users.noreply.github.com>
2021-03-18 16:42:17 +0300

    add files

--- main.cpp
+++ main.cpp
@@ -8,9 +8,12 @@
 
 using namespace std;
 
-const int N = 10;
-const char *INAME = "input.txt";
-const char *ONAME = "outputs.txt";
+const int N = 9;
+const char *INAME = "/Users/dacoder/development/untitled/input.txt";
+const char *ONAME = "/Users/dacoder/development/untitled/output.txt";
+
+///Объявление функций
+void closeStreams(ifstream& in,ofstream& out);
 
 int main(){
     //Создание потоков для чтения из файла
@@ -29,13 +32,57 @@ int main(){
     int start;
     int end;
     i=0;
+
+    //Проверка что файл открыт
+    if(!in){
+        cout << "Файл не найден." << endl;
+        //Закрытие файлов
+        closeStreams(in,out);
+        return 1;
+    }//if(!in)
+
+    //Проверка на то, что файл не пустой
+    if(in.peek() == EOF){
+        cout << "Введен пустой файл." << endl;
+        //Закрытие файлов
+        closeStreams(in,out);
+        //Выход с кодом 2
+        return 2;
+    }//if(in.peek() == EOF)
+
+    //Проверка на корректность файла.
+    if(in.fail()){
+        cout << "Ошибка при чтении файла." << endl;
+        //Закрытие файлов
+        closeStreams(in,out);
+        //Выход с кодом 3
+        return 3;
+    }//if(in.fail())
+
     //Заполнение массива числами из файла(пока не закончится файл)
     while (!in.eof()){
+        //Проверка переполнения массива
+        if(i >= N){
+            cout << "Массив должен содержать " << N << " элементов. Введено больше " << N << " элементов." << endl;
+            //Закрытие файлов
+            closeStreams(in,out);
+            //Выход с кодом 4
+            return 4;
+        }//if
+
         in >> array[i];
         cout << "array [" << i<<"] = "<<array[i] << endl;
         i++;
-    }
+    }//while
 
+    //Проверка недозаполнения массива
+    if(i < N){
+        cout << "Массив должен содержать " << N <<" элементов." << endl << "Вы ввели "<<i<<" элементов."<<endl;
+        //Закрытие файлов
+        closeStreams(in,out);
+        //Выход с кодом 5
+        return 5;
+    }//if
 
     //Инициализация переменных
     negSum = 0;
@@ -83,10 +130,21 @@ int main(){
     cout << "Минимальный элемент: " << min << " индекс: " << minI <<endl;
     cout << "Максимальный элемент: " << max << " индекс: " << maxI <<endl;
     cout << "Сумма отрицательных элементов: " << negSum << endl;
-    cout << "Произведение между наименьшим и наибольшим: " << mulBetween << endl;
+    cout << "Произведение элементов между наименьшим и наибольшим: " << mulBetween << endl;
+
+    out << "Минимальный элемент: " << min << " индекс: " << minI <<endl;
+    out << "Максимальный элемент: " << max << " индекс: " << maxI <<endl;
+    out << "Сумма отрицательных элементов: " << negSum << endl;
+    out << "Произведение элементов между наименьшим и наибольшим: " << mulBetween << endl;
 
 
     //Закрываем файлы
+   closeStreams(in,out);
+}// main()
+
+
+///Реализация функций
+void closeStreams(ifstream& in,ofstream& out){
     in.close();
     out.close();
-}// main()
+}
